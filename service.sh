5b0f01206a3abb0b3d6b876b9461a58b8b24b11f
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2020-12-16 18:52:08 +0800

    Update

--- service.sh
+++ service.sh
@@ -1,12 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+while sleep 5; do
+  if [[ -d "/sdcard/Android" ]] && [["$(getprop sys.boot_completed)"="1"]]; then
+    break
+  fi
+done
+
 FILE=/data/local/logcatcher/boot.lcs
-if [ ! -f "$FILE" ];
-then
-  pkill -f logcatcher-boot-mlgmxyysd:S
-fi
-FILE_ERR=/data/local/logcatcher/error.lcs
-if [ ! -f "$FILE_ERR" ];
-then
-  pkill -f logcatcher-boot-error-mlgmxyysd:S
+if [ ! -f "$FILE" ]; then
+  pkill -f logcatcher-bootlog:S
 fi
